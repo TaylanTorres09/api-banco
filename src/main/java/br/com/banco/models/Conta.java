2077e7bf7e9fbba2d001e525389496e71657b626
TaylanTorres09 <taylantorres@gmail.com>
2023-07-13 16:28:08 -0300

    put relationship with transferecia

--- src/main/java/br/com/banco/models/Conta.java
+++ src/main/java/br/com/banco/models/Conta.java
@@ -1,20 +1,25 @@
 package br.com.banco.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "conta")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Conta {
     
     @Id
@@ -22,5 +27,11 @@ public class Conta {
     private Long idConta;
 
     private String nomeResponsavel;
+
+    private String nomeOperadorTransacao;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "conta")
+    private List<Transferencia> transferencias = new ArrayList<>();
 
 }
