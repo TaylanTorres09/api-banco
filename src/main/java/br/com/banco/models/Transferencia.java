a9c140874850f491f202989617cbde31ee161a7a
TaylanTorres09 <taylantorres@gmail.com>
2023-07-13 16:27:45 -0300

    Create Model Transferencia with relationship Conta

---
+++ src/main/java/br/com/banco/models/Transferencia.java
@@ -0,0 +1,36 @@
+package br.com.banco.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "transferencia")
+@Data
+@AllArgsConstructor
+public class Transferencia {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dataTransferencia;
+
+    private Double valor;
+
+    private String tipo;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+
+}
