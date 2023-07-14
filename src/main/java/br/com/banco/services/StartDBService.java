f3c52d548636e1f4b9fd05c7b98df23542939055
TaylanTorres09 <taylantorres@gmail.com>
2023-07-14 18:31:39 -0300

    Include format date

--- src/main/java/br/com/banco/services/StartDBService.java
+++ src/main/java/br/com/banco/services/StartDBService.java
@@ -1,6 +1,7 @@
 package br.com.banco.services;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +25,14 @@ public class StartDBService {
         Conta contaFulano = new Conta(null, "Fulano");
         Conta contaSicrano = new Conta(null, "Sicrano");
 
-        Transferencia transferencia1 = new Transferencia(null, LocalDateTime.parse("2019-01-01 12:00:00+03"), 30895.46, "DEPOSITO", null, contaFulano);
-        Transferencia transferencia2 = new Transferencia(null, LocalDateTime.parse("2019-02-03 09:53:27+03"), 12.24, "DEPOSITO", null, contaSicrano);
-        Transferencia transferencia3 = new Transferencia(null, LocalDateTime.parse("2019-05-04 08:12:45+03"), -500.50, "SAQUE", null, contaFulano);
-        Transferencia transferencia4 = new Transferencia(null, LocalDateTime.parse("2019-08-07 08:12:45+03"), -530.50, "SAQUE", null, contaSicrano);
-        Transferencia transferencia5 = new Transferencia(null, LocalDateTime.parse("2020-06-08 10:15:01+03"), 3241.23, "TRANSFERENCIA", "Beltrano", contaFulano);
-        Transferencia transferencia6 = new Transferencia(null, LocalDateTime.parse("2021-04-01 12:12:04+03"), 25173.09, "TRANSFERENCIA", "Ronnyscley", contaSicrano);
+        DateTimeFormatter fmt =  DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        
+        Transferencia transferencia1 = new Transferencia(null, LocalDateTime.parse("2019-01-01 12:00:00", fmt), 30895.46, "DEPOSITO", null, contaFulano);
+        Transferencia transferencia2 = new Transferencia(null, LocalDateTime.parse("2019-02-03 09:53:27", fmt), 12.24, "DEPOSITO", null, contaSicrano);
+        Transferencia transferencia3 = new Transferencia(null, LocalDateTime.parse("2019-05-04 08:12:45", fmt), -500.50, "SAQUE", null, contaFulano);
+        Transferencia transferencia4 = new Transferencia(null, LocalDateTime.parse("2019-08-07 08:12:45", fmt), -530.50, "SAQUE", null, contaSicrano);
+        Transferencia transferencia5 = new Transferencia(null, LocalDateTime.parse("2020-06-08 10:15:01", fmt), 3241.23, "TRANSFERENCIA", "Beltrano", contaFulano);
+        Transferencia transferencia6 = new Transferencia(null, LocalDateTime.parse("2021-04-01 12:12:04", fmt), 25173.09, "TRANSFERENCIA", "Ronnyscley", contaSicrano);
 
         this.contaRepository.saveAll(Arrays.asList(contaFulano, contaSicrano));
         this.transferenciaRepository.saveAll(Arrays.asList(transferencia1, transferencia2, transferencia3, transferencia4, transferencia5, transferencia6));
