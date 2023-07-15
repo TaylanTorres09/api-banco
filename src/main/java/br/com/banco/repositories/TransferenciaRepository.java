01627f2ebc38c4f04a0cddf7a846196513390ce1
TaylanTorres09 <taylantorres@gmail.com>
2023-07-15 13:47:29 -0300

    Query to find Transferencia por conta id

--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -1,6 +1,12 @@
 package br.com.banco.repositories;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.banco.models.Transferencia;
@@ -8,4 +14,7 @@ import br.com.banco.models.Transferencia;
 @Repository
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
     
+    @Query(value = "SELECT * FROM TRANSFERENCIA t WHERE t.conta_id = :contaId", nativeQuery = true)
+    List<Transferencia> findByContaId(@Param(value = "contaId") Long contaId);
+
 }
