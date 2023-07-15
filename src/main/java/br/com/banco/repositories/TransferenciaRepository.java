af53f0333980a7932d7d2fcfd48e735f12c6aa03
TaylanTorres09 <taylantorres@gmail.com>
2023-07-15 13:49:13 -0300

    Remove never user libs

--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -1,8 +1,6 @@
 package br.com.banco.repositories;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
